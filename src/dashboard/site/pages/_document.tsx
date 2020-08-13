b5c18c63ae26401b8a2ff21a2f7659816ce3e88b
Nafis <nafis.shah@gmail.com>
2020-08-12 21:24:08 -0400

    stuff

--- src/dashboard/site/pages/_document.tsx
+++ src/dashboard/site/pages/_document.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Document, { Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheets } from '@material-ui/core/styles'
 import { theme } from '../components/theme'
 
@@ -7,7 +7,7 @@ import { theme } from '../components/theme'
 export default class MyDocument extends Document {
   render() {
     return (
-      <html lang="en">
+      <Html lang="en">
         <Head>
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
@@ -24,11 +24,13 @@ export default class MyDocument extends Document {
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     )
   }
 }
 
+// `getInitialProps` belongs to `_document` (instead of `_app`),
+// it's compatible with server-side generation (SSG).
 MyDocument.getInitialProps = async (ctx) => {
   // Resolution order
   //
