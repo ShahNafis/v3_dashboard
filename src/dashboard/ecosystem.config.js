81df90d8d811db76595cc6fb19f5f3f96dd369a6
Nafis <nafis.shah@gmail.com>
2020-08-13 11:17:18 -0400

    format

--- src/dashboard/ecosystem.config.js
+++ src/dashboard/ecosystem.config.js
@@ -1,13 +1,15 @@
 module.exports = {
-    apps : [{
-        name: "app",
-        script: "./dist/index.js",
-        env: {
-          NODE_ENV: "production",
-        },
-        error_file: 'err.log',
-        out_file: 'out.log',
-        log_file: 'combined.log',
-        time: true
-    }]
-};
+  apps: [
+    {
+      name: 'app',
+      script: './dist/index.js',
+      env: {
+        NODE_ENV: 'production',
+      },
+      error_file: 'err.log',
+      out_file: 'out.log',
+      log_file: 'combined.log',
+      time: true,
+    },
+  ],
+}
